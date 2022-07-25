1b4bdc05bdf9a98a7c235c7b7d89b4c55259894d
unknown <willhc1984@gmail.com>
2022-07-25 15:50:41 -0300

    Inicio de projeto com IDE STS 4

---
+++ src/test/java/com/dio/academiadigital/AcademiaDigitalDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.academiadigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaDigitalDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
