c2a6c5a81dc9532667d67b8de8b6d9d8a6e08e50
unknown <willhc1984@gmail.com>
2022-07-26 00:19:28 -0300

    Camada Service - AvaliacaoFisicaService

---
+++ src/main/java/com/dio/academiadigital/entities/form/AvaliacaoFisicaForm.java
@@ -0,0 +1,28 @@
+package com.dio.academiadigital.entities.form;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AvaliacaoFisicaForm {
+	
+	@Positive(message = "O Id do aluno precisa ser positivo.")
+	private Long alunoId;
+	
+	@NotNull(message = "Preencha o campo corretamente.")
+	@Positive(message = "${validatedValue}' precisa ser positivo.")
+	private Double peso;
+	
+	@NotNull(message = "Preencha o campo corretamente.")
+	@Positive(message = "${validatedValue}' precisa ser positivo.")
+	@DecimalMin(value = "150", message = "'${validatedValue}' precisa ser no mínimo {value}.")
+	private Double altura;
+	
+}
---
+++ src/main/java/com/dio/academiadigital/services/AvaliacaoFisicaService.java
@@ -0,0 +1,62 @@
+package com.dio.academiadigital.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dio.academiadigital.entities.Aluno;
+import com.dio.academiadigital.entities.AvaliacaoFisica;
+import com.dio.academiadigital.entities.form.AvaliacaoFisicaForm;
+import com.dio.academiadigital.repositories.AlunoRepository;
+import com.dio.academiadigital.repositories.AvaliacaoFisicaRepository;
+
+@Service
+public class AvaliacaoFisicaService {
+	
+	@Autowired
+	private AvaliacaoFisicaRepository avaliacaoFisicaRepository;
+	
+	@Autowired
+	private AlunoRepository alunoRepository;
+	
+	public List<AvaliacaoFisica> getAll(){
+		List<AvaliacaoFisica> avaliacoes = avaliacaoFisicaRepository.findAll();
+		return avaliacoes;
+	}
+	
+	public AvaliacaoFisica getById(Long id) {
+		Optional<AvaliacaoFisica> avaliacaoFisica = avaliacaoFisicaRepository.findById(id);
+		return avaliacaoFisica.get();
+	}
+	
+	public AvaliacaoFisica create(AvaliacaoFisicaForm form) {
+		AvaliacaoFisica avaliacaoFisica = new AvaliacaoFisica();
+		Aluno aluno = alunoRepository.findById(form.getAlunoId()).get();
+		
+		avaliacaoFisica.setAluno(aluno);
+		avaliacaoFisica.setAltura(form.getPeso());
+		avaliacaoFisica.setPeso(form.getPeso());
+		
+		return avaliacaoFisicaRepository.save(avaliacaoFisica);
+	}
+	
+	public void delete(Long id) {
+		avaliacaoFisicaRepository.deleteById(id);
+	}
+	
+	public AvaliacaoFisica update(Long id, AvaliacaoFisica avaliacaoFisica) {
+		@SuppressWarnings("deprecation")
+		AvaliacaoFisica obj = avaliacaoFisicaRepository.getOne(id);
+		updateData(obj, avaliacaoFisica);
+		return avaliacaoFisicaRepository.save(obj);
+	}
+
+	private void updateData(AvaliacaoFisica obj, AvaliacaoFisica avaliacaoFisica) {
+		obj.setPeso(avaliacaoFisica.getPeso());
+		obj.setAltura(avaliacaoFisica.getAltura());
+		obj.setDataDaAvaliacao(avaliacaoFisica.getDataDaAvaliacao());
+	}
+
+}
