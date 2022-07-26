41bba118479bffc036b5441b168054e493321484
unknown <willhc1984@gmail.com>
2022-07-26 00:00:06 -0300

    AlunoControlelr - delete/put

--- src/main/java/com/dio/academiadigital/resources/AlunoController.java
+++ src/main/java/com/dio/academiadigital/resources/AlunoController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,9 +31,25 @@ public class AlunoController {
 		return alunos;
 	}
 	
+	@GetMapping(value = "/{id}")
+	public Aluno getById(@PathVariable Long id) {
+		Aluno aluno = service.getById(id);
+		return aluno;
+	}
+	
 	@PostMapping
 	public Aluno create(@Valid @RequestBody AlunoForm form) {
 		return service.create(form);
 	}
-
+	
+	@PutMapping(value = "/{id}")
+		public Aluno update(@RequestBody Aluno aluno, @PathVariable Long id) {
+			service.update(id, aluno);
+			return aluno;
+		}
+	
+	@DeleteMapping(value = "/{id}")
+	public void deleteById(@PathVariable Long id) {
+		service.delete(id);
+	}
 }
