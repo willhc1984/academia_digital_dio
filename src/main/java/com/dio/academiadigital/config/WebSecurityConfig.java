4170ef26eb966f2b4e7085298534348bd5810ce3
unknown <willhc1984@gmail.com>
2022-08-02 14:40:41 -0300

    Autenticação basica em memoria - SpringSecurity

---
+++ src/main/java/com/dio/academiadigital/config/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.dio.academiadigital.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}user123")
+                .roles("USERS")
+                .and()
+                .withUser("admin")
+                .password("{noop}admin123")
+                .roles("MANAGERS");
+    }
+
+}
